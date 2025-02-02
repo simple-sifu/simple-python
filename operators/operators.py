99da32169829c1ba10fffc3e3716f13126ee8b3c
Tommy Han <tommy.han@tommyha-ltmdjbu.internal.salesforce.com>
2025-02-02 00:30:40 -0500

    add assignment, comparison, operators

---
+++ operators/operators.py
@@ -0,0 +1,12 @@
+x = 10
+y = 5
+
+print ("The value after addition is: ", x+y)
+print ("The value after subtraction is: ", x-y)
+print ("The value after multiplication is: ", x*y)
+print ("The value after division is: ", x/y)
+print ("The value after floor division is: ", x//y) 
+print ("The modules value after division is: ", x%y)
+print ("The remainder value after division is: ", x**y)
+
+
